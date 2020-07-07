176f20e7a142b6430fa76f8b68c85dec09b5df38
ferdinand ismael <ferdinand.ismael@gmail.com>
2020-07-07 09:08:35 -0400

    included saga tests

--- sagas.js
+++ sagas.js
@@ -1,23 +1,30 @@
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
 
 // Sagas are implemented as Generator functions that yield objects to the redux-saga middleware.
 // When a Promise is yielded to the middleware, the middleware will suspend the Saga until the Promise completes
-const delay = ms => new Promise(res => setTimeout(res, ms))
+export const delay = ms => new Promise(res => setTimeout(res, ms))
 
 
-function* helloSaga() {
+export function* helloSaga() {
     console.log('Hello Saga!')
 }
 
 // Our worker Saga: will perform the async increment task
-function* incrementAsync() {
+export function* incrementAsync() {
     // suspend until the Promise returned by delay resolves
-    yield delay(1000)
+    // we use call here to pass call(delay, 100) to the caller of next
+    // useful for tests
+    // call returns an effect which instructs the middleware to call a given functiom with a given arguement
+    // put nor call performs any dispatch or asynchronous call by themselves, they return plain JavaScript objects
+    yield call(delay, 1000) // returns => { CALL: { fn: delay, args: [1000] } }
     // instructs the middleware to dispatch an INCREMENT action
     // put is one example of what we call an Effect
     // Effects are plain JavaScript objects which contain instructions to be fulfilled by the middleware
     // When a middleware retrieves an Effect yielded by a Saga, the saga is paused until the Efffect is fulfilled
-    yield put({ type: 'INCREMENT' })
+    yield put({ type: 'INCREMENT' }) // returns { PUT: { type: 'INCREMENT' } }
+    // What happens is that the middleware examines the type of each yielded Effect then decides how to fulfill that Effect.
+    // If the Effect type is a PUT then it will dispatch an action to the Store. 
+    // If the Effect is a CALL then it'll call the given function.
 }
 
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
